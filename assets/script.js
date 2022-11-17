9bbfb8f6854e14b5040e4705cb208812f2cace1f
Valerii Kryshtal <112430151+ValeriiKryshtal@users.noreply.github.com>
2022-11-17 18:07:00 -0400

    added first version of code

---
+++ assets/script.js
@@ -0,0 +1,86 @@
+const DATA = [
+    {
+        question: 'Question-1',
+        answers: [
+            {
+                id: '1',
+                value: 'Answer-1',
+                correct: true,
+            },
+            {
+                id: '2',
+                value: 'Answer-1',
+                correct: true,
+            },
+            {
+                id: '3',
+                value: 'Answer-1',
+                correct: true,
+            },
+            {
+                id: '4',
+                value: 'Answer-1',
+                correct: true,
+            },
+        ]
+    }
+]
+
+const quiz = document.querySelector('.quiz');
+const questions = 
+const indicator = 
+const results = 
+const buttonNext = 
+const buttonRestart = 
+
+const buttons = document.querySelectorAll('.btn');
+
+const mainContainer1 = document.getElementById("main-container-1"); 
+const mainContainer2 = document.getElementById("main-container-2");
+const bottomWrong = document.getElementById("wrong");
+const bottomCorrect = document.getElementById("correct");
+
+const button1 = document.getElementById('btn1');
+const button2 = document.getElementById('btn2');
+const button3 = document.getElementById('btn3');
+const button4 = document.getElementById('btn4');
+ 
+let but1 = Boolean(button1);
+let but2 = Boolean(button2);
+let but3 = Boolean(button3);
+let but4 = Boolean(button4);
+
+
+
+
+
+
+
+
+
+
+
+//function pageSwitch() {
+//
+//    function displayContainer() {
+//        mainContainer1.style.display = 'none';
+//        mainContainer2.style.display = 'inline'; 
+//    }
+//    
+//    showAnswer();  
+ //   function showAnswer () {
+    
+//        console.log(but1);
+ //       if (but1) {
+   //         bottomCorrect.style.display = 'inline'
+  //      }
+  //      else {
+   //         bottomWrong.style.display = 'inline'
+  //      }   
+  //  }
+    
+  //  setTimeout(displayContainer, 3000);      
+//}
+//buttons.forEach(buttonsItem => {buttonsItem.addEventListener("click", pageSwitch)}); 
+
+
